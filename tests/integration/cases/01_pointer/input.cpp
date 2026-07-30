dfd079d0e6827521127f266a5a1cc15a35952c6c
yy <736262857@qq.com>
2026-07-30 08:58:29 +0000

    test(pointer): 添加复杂模板引用声明用例 std::vector<char>& put

--- tests/integration/cases/01_pointer/input.cpp
+++ tests/integration/cases/01_pointer/input.cpp
@@ -5,6 +5,7 @@
 void process(int* data, double& value);
 void transform(const char** args, int & count);
 char* createBuffer(size_t* size);
+void input(std::vector<char>& put);
 
 int main() {
     int    value = 42;
